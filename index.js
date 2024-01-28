df44c8cde9f69bb62fa7c38f9556354c8c57615a
Saniya98 <saniyafarheen.98@gmail.com>
2024-01-28 17:39:28 -0500

    Third commit

--- index.js
+++ index.js
@@ -49,24 +49,30 @@ function makesound(key) {
       tom4.play();
       break;
 
-      case "w":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
       case "a":
         var kick = new Audio("sounds/kick-bass.mp3");
         kick.play();
         break;
 
-      case "s":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      default: console.log(buttonInnerHTML)
-        break;
-    }
-  });
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
 }
-// var audio = new Audio('sounds/tom-1.mp3');
-//     audio.play();
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 200);
+}
